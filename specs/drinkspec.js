fee5af9d7140f21f2a74f614bf0d50175f8f9613
Andrew Burns <burnsad@tcd.ie>
2020-05-22 09:57:20 +0000

    reviewing jasmine

---
+++ specs/drinkspec.js
@@ -0,0 +1,8 @@
+describe("drink about challenge", function(){
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    }); 
+})
+describe("checks age", function(){
+    it("should be checked")
+})
